8f7cec7e6ae7820aeac95e4b4bbcc81c44f8194e
coutinho98 <mateuscouto98@outlook.com>
2025-07-15 13:15:07 -0300

    ..

---
+++ App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-2xl font-bold">Hi Shadys :)</h1>
+    </div>
+  )
+}
+
+export default App;
